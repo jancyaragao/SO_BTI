924272ec8bd27b268845e96b95401f9f7cad552e
Jâncy Aragão <jancy.aragao@gmail.com>
2019-09-09 18:03:42 -0300

    Adição dos documentos referentes ao projeto

---
+++ Trabalho Prático 1/unroll.py
@@ -0,0 +1,10 @@
+def func(var1, var2):
+    print (str(var1) + ", " + str(var2))
+
+def unroll(args, func, results, method):
+    if method:
+        pass
+
+res = []
+unroll(, func, "fork", res)
+unroll(, func, "threads", res)
