0e380ec4b0695715bf0565e5fe4bfd95523c1b8e
carvalho <carvalho.pedro.cardoso@gmail.com>
2019-09-08 22:04:57 -0300

    somar so co thread

--- somar.py
+++ somar.py
@@ -3,16 +3,7 @@ import random
 import threading
 
 sem = threading.Lock()
-def random_matrix(rows, cols):
-    matrix = []
-    for i in range(rows):
-        matrix.append([])
-        for j in range(cols):
-            matrix[i].append([])
-            matrix[i][j] = random.randint(0, 9)
-    return matrix
-
-# def sum_proc(row_a, row_b, results):
+# def somar_proc(row_a, row_b, results):
 #     pid = os.fork() 
 #     if pid == 0: 
 #         aux = [] 
@@ -21,7 +12,7 @@ def random_matrix(rows, cols):
 
 #         results.append(aux)  
 
-def sum_thre(i, j, a, b, results):
+def somar_thre(i, j, a, b, results):
     threading.currentThread() 
     results[i][j] = a + b
 
@@ -32,42 +23,42 @@ def print_matrix(matrix):
         print()
 
 def unroll(args, func, method, results):
-    random_m = random_matrix(len(args), len(args[0]))
-    print("Matriz Original")
-    print_matrix(args)
-    print("Matriz radom\n")
-    print_matrix(random_m)
+    matrix1 = args[0]
+    matrix2 = args[1]
+    print("Primeira Matriz")
+    print_matrix(matrix1)
+    print("Segunda Matriz")
+    print_matrix(matrix2)
 
     if method == "thre":
         threads = []
-        cols = len(args[0])
-        rows = len(args)
-
+        cols = len(matrix1[0])
+        rows = len(matrix1)
         results = [[0 for i in range(cols)] for j in range(rows)]
+        
         for i in range(rows):
             for j in range(cols):
                 threads.append([])
-                threads[-1] = threading.Thread(target=func, args=(i, j, args[i][j], random_m[i][j], results))
+                threads[-1] = threading.Thread(target=func, args=(i, j, matrix1[i][j], matrix2[i][j], results))
                 threads[-1].start()
         for i in range(rows):
             for j in range(cols):
-                # threads.append([])
-                # threads[-1] = threading.Thread(target=func, args=(i, j, args[i][j], random_m[i][j], results))
                 threads[i+j].join()
         sem.acquire() 
         print("Matriz Resultante\n")     
-        # res = results
         print_matrix(results)
         sem.release()
 
     else: 
-        for arg, rand in zip(args, random_m):
+        for arg, rand in zip(matrix1,matrix2):
             func(arg, rand, results)            
 
         print_matrix(results)
 
 if __name__ == '__main__':
     res = []
-    # unroll([[0,1,2],[3,4,5],[6,7,8]], sum_proc, 'proc', res)
-    unroll([[0,1,2],[3,4,5],[6,7,8]], sum_thre, 'thre', res)
+    matrix1 = [[0,1,2],[3,4,5],[6,7,8]]
+    matrix2 = [[8,7,6],[5,4,3],[2,1,0]]
+    # unroll([matrix1,matrix2], somar_proc, 'proc', res)
+    unroll([matrix1,matrix2], somar_thre, 'thre', res)
     print_matrix(res)
